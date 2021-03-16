9cc9f3920cdd1ba3a4015cbc7c6c29070afef1a8
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-03-16 16:14:00 +0100

    rób julka

--- Zad1/SignalAndImpulseCreator.py
+++ Zad1/SignalAndImpulseCreator.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from Signal.Signal import Signal
 from SignalAndImpulse import SignalAndImpulse
 
 
@@ -8,3 +9,15 @@ def create_signal_and_impulse(signal: SignalAndImpulse, start_time: float, end_t
     for i in np.arange(start_time, end_time, delta):
         data[i] = signal.generate_value(i)
     return data
+
+
+class RENAME_ME():
+    is_signal: bool
+    time_values_dict: dict
+
+    def __init__(self, signal_and_impulse: SignalAndImpulse, start_time: float, end_time: float, delta: float):
+        if isinstance(signal_and_impulse, Signal):
+            self.is_signal = True
+        else:
+            self.is_signal = False
+        self.time_values_dict = create_signal_and_impulse(signal_and_impulse, start_time, end_time, delta)
