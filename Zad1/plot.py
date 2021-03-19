5360511567f5d5c301cec9662627d6306a242185
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-03-19 14:39:28 +0100

    Added histogram

--- Zad1/plot.py
+++ Zad1/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from SignalAndImpulseCreator import SignalData
 
 
@@ -7,12 +8,19 @@ def plot_analog(signal_dictionary: dict):
     array_from_list = np.asarray(list(signal_dictionary.items()))
     plt.plot(array_from_list[:, 0], array_from_list[:, 1])
     plt.show()
+    unique = np.unique(array_from_list[:, 1])
+    plt.hist(array_from_list[:, 1], bins=len(unique))
+    plt.show()
 
 
 def plot_discrete(impulse_dictionary: dict):
     array_from_list = np.asarray(list(impulse_dictionary.items()))
     plt.scatter(array_from_list[:, 0], array_from_list[:, 1])
     plt.show()
+    unique = np.unique(array_from_list[:, 1])
+    plt.hist(array_from_list[:, 1], bins=len(unique))
+    plt.show()
+
 
 
 def plot(data: SignalData):
